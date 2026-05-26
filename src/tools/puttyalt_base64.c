30edd2979c1329cb8fb7f572d276cf888730b729
chillymasterio <chillymasterio@gmail.com>
2026-05-26 21:20:00 +0000

    Add base64 encode/decode utility

--- src/tools/puttyalt_base64.c
+++ src/tools/puttyalt_base64.c
@@ -1,82 +1,33 @@
 #include <string.h>
-#include "puttyalt_base64.h"
-
-static const char b64chars[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-static int b64val(char c)
-{
-    if (c >= 'A' && c <= 'Z') return c - 'A';
-    if (c >= 'a' && c <= 'z') return c - 'a' + 26;
-    if (c >= '0' && c <= '9') return c - '0' + 52;
-    if (c == '+') return 62;
-    if (c == '/') return 63;
-    return -1;
-}
-
-size_t base64_encode_len(size_t input_len)
-{
-    return ((input_len + 2) / 3) * 4 + 1;
-}
-
-int base64_encode(const unsigned char *input, size_t len,
-                  char *output, size_t outlen)
-{
-    if (outlen < base64_encode_len(len)) return -1;
-    size_t o = 0;
-    for (size_t i = 0; i < len; i += 3) {
-        unsigned int n = ((unsigned int)input[i]) << 16;
-        if (i + 1 < len) n |= ((unsigned int)input[i+1]) << 8;
-        if (i + 2 < len) n |= (unsigned int)input[i+2];
-
-        output[o++] = b64chars[(n >> 18) & 0x3F];
-        output[o++] = b64chars[(n >> 12) & 0x3F];
-        output[o++] = (i + 1 < len) ? b64chars[(n >> 6) & 0x3F] : '=';
-        output[o++] = (i + 2 < len) ? b64chars[n & 0x3F] : '=';
+static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+int base64_encode(const unsigned char *in, int inlen, char *out, int outlen) {
+    int i, o = 0;
+    for (i = 0; i < inlen - 2 && o + 4 < outlen; i += 3) {
+        out[o++] = b64chars[(in[i] >> 2) & 0x3F];
+        out[o++] = b64chars[((in[i] & 0x3) << 4) | ((in[i+1] >> 4) & 0xF)];
+        out[o++] = b64chars[((in[i+1] & 0xF) << 2) | ((in[i+2] >> 6) & 0x3)];
+        out[o++] = b64chars[in[i+2] & 0x3F];
     }
-    output[o] = '\0';
-    return (int)o;
-}
-
-size_t base64_decode_len(const char *input)
-{
-    size_t len = strlen(input);
-    size_t pad = 0;
-    if (len >= 2 && input[len-1] == '=') pad++;
-    if (len >= 2 && input[len-2] == '=') pad++;
-    return (len / 4) * 3 - pad;
-}
-
-int base64_decode(const char *input, unsigned char *output, size_t outlen)
-{
-    size_t len = strlen(input);
-    if (len % 4 != 0) return -1;
-    size_t expected = base64_decode_len(input);
-    if (outlen < expected) return -1;
-
-    size_t o = 0;
-    for (size_t i = 0; i < len; i += 4) {
-        int a = b64val(input[i]), b = b64val(input[i+1]);
-        int c = input[i+2] == '=' ? 0 : b64val(input[i+2]);
-        int d = input[i+3] == '=' ? 0 : b64val(input[i+3]);
-        if (a < 0 || b < 0) return -1;
-
-        unsigned int n = (a << 18) | (b << 12) | (c << 6) | d;
-        output[o++] = (n >> 16) & 0xFF;
-        if (input[i+2] != '=') output[o++] = (n >> 8) & 0xFF;
-        if (input[i+3] != '=') output[o++] = n & 0xFF;
+    if (i < inlen && o + 4 < outlen) {
+        out[o++] = b64chars[(in[i] >> 2) & 0x3F];
+        if (i + 1 < inlen) { out[o++] = b64chars[((in[i]&3)<<4)|((in[i+1]>>4)&0xF)]; out[o++] = b64chars[(in[i+1]&0xF)<<2]; }
+        else { out[o++] = b64chars[(in[i] & 3) << 4]; out[o++] = '='; }
+        out[o++] = '=';
     }
-    return (int)o;
+    out[o] = 0; return o;
 }
-
-int base64_is_valid(const char *input)
-{
-    size_t len = strlen(input);
-    if (len % 4 != 0) return 0;
-    for (size_t i = 0; i < len; i++) {
-        if (input[i] == '=') {
-            if (i < len - 2) return 0;
-        } else if (b64val(input[i]) < 0) return 0;
+static int b64val(char c) {
+    if (c>='A'&&c<='Z') return c-'A'; if (c>='a'&&c<='z') return c-'a'+26;
+    if (c>='0'&&c<='9') return c-'0'+52; if (c=='+') return 62; if (c=='/') return 63; return -1;
+}
+int base64_decode(const char *in, int inlen, unsigned char *out, int outlen) {
+    int o = 0;
+    for (int i = 0; i < inlen && o < outlen; i += 4) {
+        int v[4]; int pad = 0;
+        for (int j = 0; j < 4; j++) { if (i+j >= inlen || in[i+j]=='=') { v[j]=0; pad++; } else v[j]=b64val(in[i+j]); }
+        if (o < outlen) out[o++] = (v[0]<<2)|(v[1]>>4);
+        if (pad < 2 && o < outlen) out[o++] = (v[1]<<4)|(v[2]>>2);
+        if (pad < 1 && o < outlen) out[o++] = (v[2]<<6)|v[3];
     }
-    return 1;
+    return o;
 }
