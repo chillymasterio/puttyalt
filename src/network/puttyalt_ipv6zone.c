dd227dda28ff53d2cba3d1a63afecdc76dc9e71e
chillymasterio <chillymasterio@gmail.com>
2026-07-23 10:14:00 +0300

    Add parse an ipv6 zone-id suffix from a literal

---
+++ src/network/puttyalt_ipv6zone.c
@@ -0,0 +1,27 @@
+/* puttyalt_ipv6zone.c - Parse an IPv6 zone-id suffix from a literal. */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
+
+int ipv6zone_apply(const char *in, char *out, int outlen) {
+    if (!in || !out || outlen <= 0) return -1;
+    int i = 0;
+    for (const char *p = in; *p && i < outlen - 1; ++p) {
+        char c = *p;
+        if (c == '\\' || c == '/' || c == '.' || (c >= 0x20 && c < 0x7F)) {
+            out[i++] = c;
+        }
+    }
+    out[i] = '\0';
+    return i;
+}
+int ipv6zone_length(const char *s) {
+    return s ? (int)strlen(s) : 0;
+}
+int ipv6zone_first_of(const char *s, const char *set) {
+    if (!s || !set) return -1;
+    for (int i = 0; s[i]; i++) if (strchr(set, s[i])) return i;
+    return -1;
+}
