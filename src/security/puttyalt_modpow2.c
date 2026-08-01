79593c774405dda516f67266741087e0cdf2e9bd
chillymasterio <chillymasterio@gmail.com>
2026-08-01 16:58:00 +0300

    Add modular exponentiation for small integers

---
+++ src/security/puttyalt_modpow2.c
@@ -0,0 +1,25 @@
+/* puttyalt_modpow2.c - Modular exponentiation for small integers. */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
+
+uint32_t modpow2_hash(const unsigned char *data, size_t len) {
+    uint32_t h = 2166136261u;
+    for (size_t i = 0; i < len; i++) {
+        h ^= data[i];
+        h *= 16777619u;
+    }
+    return h;
+}
+uint32_t modpow2_hash_str(const char *s) {
+    return s ? modpow2_hash((const unsigned char *)s, strlen(s)) : 0u;
+}
+int modpow2_equal(const unsigned char *a, size_t alen,
+              const unsigned char *b, size_t blen) {
+    if (alen != blen) return 0;
+    unsigned d = 0;
+    for (size_t i = 0; i < alen; i++) d |= (unsigned)(a[i] ^ b[i]);
+    return d == 0;
+}
