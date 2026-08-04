188be8c5afe5f2c8a8e838fca9949ecbe8c0531a
chillymasterio <chillymasterio@gmail.com>
2026-08-04 11:33:00 +0300

    Add render the version/build banner for about box

---
+++ src/ui/puttyalt_aboutversion.c
@@ -0,0 +1,27 @@
+/* puttyalt_aboutversion.c - Render the version/build banner for About box. */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
+
+int aboutversion_apply(const char *in, char *out, int outlen) {
+    if (!in || !out || outlen <= 0) return -1;
+    int i = 0;
+    for (const char *p = in; *p && i < outlen - 1; ++p) {
+        char c = *p;
+        if (c == '\\' || c == '/' || c == '.' || (c >= 0x20 && c < 0x7F)) {
+            out[i++] = c;
+        }
+    }
+    out[i] = '\0';
+    return i;
+}
+int aboutversion_length(const char *s) {
+    return s ? (int)strlen(s) : 0;
+}
+int aboutversion_first_of(const char *s, const char *set) {
+    if (!s || !set) return -1;
+    for (int i = 0; s[i]; i++) if (strchr(set, s[i])) return i;
+    return -1;
+}
